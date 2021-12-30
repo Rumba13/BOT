77b691f16f4863a32a20eff6876f1bce418f2167
Rumba13 <92682676+Rumba13@users.noreply.github.com>
2021-12-30 14:26:08 +0300

    Update js.js

--- js.js
+++ js.js
@@ -6,8 +6,6 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Я т11888ебе помогать не обязан'))
 bot.help((ctx) => ctx.reply('Я т11ебе помогать не обязан'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-var express = require('express');
-var app = express();
 
 const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=5&tags=genshin_impact&json=1&id=`
 let tags = ''
@@ -15,15 +13,6 @@ let r34Folder = []
 let body = { mode: 'no-cors' }
 let i = 1
 
-//! WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK
-app.set('port', (process.env.PORT || 5000));
-app.get('/', function (request, response) {
-	var result = 'App is running'
-	response.send(result);
-}).listen(app.get('port'), function () {
-	console.log('App is running, server is listening on port ', app.get('port'));
-});
-//!WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK-WEBHOOK
 
 bot.hears('lapit', (ctx) => ctx.reply('НИХУ1!!1111 нЕ АДНАГО 99919	888слова блядь'))
 bot.hears('Влад лох', (ctx) => ctx.reply('Пошёл нахуй пидарсич Сам лох чмо ебАне'))
